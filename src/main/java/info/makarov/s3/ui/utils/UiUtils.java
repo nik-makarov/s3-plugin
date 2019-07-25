f87746da0f5bec42add276cb1ddce3319c370481
Nikita Makarov <nik_makarov@yahoo.com>
2019-07-25 23:46:23 +0300

    Handle default application not found exception

--- src/main/java/info/makarov/s3/ui/utils/UiUtils.java
+++ src/main/java/info/makarov/s3/ui/utils/UiUtils.java
@@ -2,10 +2,10 @@ package info.makarov.s3.ui.utils;
 
 import com.intellij.openapi.fileChooser.FileChooser;
 import com.intellij.openapi.fileChooser.FileChooserDescriptor;
+import com.intellij.openapi.ui.Messages;
 import com.intellij.openapi.vfs.VirtualFile;
 import com.intellij.util.Consumer;
 import info.makarov.s3.core.entity.NodeModel;
-import info.makarov.s3.core.exception.S3PluginException;
 import info.makarov.s3.ui.models.S3TreeNode;
 import lombok.experimental.UtilityClass;
 
@@ -28,8 +28,7 @@ public class UiUtils {
         try {
             Desktop.getDesktop().open(file);
         } catch (IOException e) {
-            // TODO Show default application not found dialog(Error code: -10814)
-            throw new S3PluginException("Cannot to open file " + file, e);
+            Messages.showWarningDialog("Default application for " + file.getName() + " not found", "Sorry");
         }
     }
 
